dc4ec79f4e242340d72a1a93c4e7849c8bde02f9
krzysztofkarpieszuk <krzysztof.karpieszuk@gmail.com>
2018-10-26 13:32:06 +0200

    Presentation - product version

---
+++ my-recipes-app/docs/precache-manifest.f5bda21752e6915082266ebfeda96ff6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f4d56b99dce96126a60dd91385245d3",
+    "url": "./static/media/search-bg-tiny.8f4d56b9.jpg"
+  },
+  {
+    "revision": "dfe065c1f5fc313150c1636f80887f8a",
+    "url": "./static/media/recipe-box-bg-tiny.dfe065c1.jpg"
+  },
+  {
+    "revision": "e8b73ff9a1982335b529f54501b8feeb",
+    "url": "./static/media/home-page-bg-tiny.e8b73ff9.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "dc0e848cd97339a6b71d",
+    "url": "./static/js/main.dc0e848c.chunk.js"
+  },
+  {
+    "revision": "77f9f2547e777f61b8a1",
+    "url": "./static/js/1.77f9f254.chunk.js"
+  },
+  {
+    "revision": "dc0e848cd97339a6b71d",
+    "url": "./static/css/main.ba23c161.chunk.css"
+  },
+  {
+    "revision": "459aa0da0b42d17b1c23feb64eb13ebb",
+    "url": "./index.html"
+  }
+];
