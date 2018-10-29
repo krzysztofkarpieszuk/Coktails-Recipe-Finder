d1249e68631fa78b908bdc4e3817d5762676d05d
krzysztofkarpieszuk <krzysztof.karpieszuk@gmail.com>
2018-10-29 11:50:54 +0100

    Updated Version with few bugs fixed

---
+++ docs/build/precache-manifest.f37cd74c4424d9abab2e1117e2aeb082.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f4d56b99dce96126a60dd91385245d3",
+    "url": "./static/media/search-bg-tiny.8f4d56b9.jpg"
+  },
+  {
+    "revision": "dfe065c1f5fc313150c1636f80887f8a",
+    "url": "./static/media/recipe-box-bg-tiny.dfe065c1.jpg"
+  },
+  {
+    "revision": "e8b73ff9a1982335b529f54501b8feeb",
+    "url": "./static/media/home-page-bg-tiny.e8b73ff9.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b9ced7d895ff17d318cf",
+    "url": "./static/js/main.b9ced7d8.chunk.js"
+  },
+  {
+    "revision": "4262b5888ea637776a92",
+    "url": "./static/js/1.4262b588.chunk.js"
+  },
+  {
+    "revision": "b9ced7d895ff17d318cf",
+    "url": "./static/css/main.ad1646fd.chunk.css"
+  },
+  {
+    "revision": "f35481f0ff5eb3a9ae3f8cddc3b6c935",
+    "url": "./index.html"
+  }
+];
