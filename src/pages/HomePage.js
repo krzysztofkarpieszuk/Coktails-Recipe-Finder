8c6778b75abf4ed2842ff4a619c0afb06da47f89
krzysztofkarpieszuk <krzysztof.karpieszuk@gmail.com>
2018-11-14 15:29:44 +0100

    Installed Slider and Imported to Home Page - To Customize

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import Slider from 'react-slick';
 
+const HomeSlider = () => {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 800,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		arrows: false,
+		breakpoints: [800, 550, 420, 330]
+	}
+
+	return (
+
+		<Slider {...settings} >
+			<div>
+				<h2 className="app-home__title">Welcome to Drinkello</h2>
+				<p className="app-home__description">
+				Drinkello is a recipe finder for classic cocktails. Please select an option from menu to get started.
+				</p>
+			</div>
+			<div>
+				<h2 className="app-home__title">Welcome to Drinkello</h2>
+				<p className="app-home__description">
+				Drinkello is a recipe finder for classic cocktails. Please select an option from menu to get started.
+				</p>
+			</div>
+			<div>
+				<h2 className="app-home__title">Welcome to Drinkello</h2>
+				<p className="app-home__description">
+				Drinkello is a recipe finder for classic cocktails. Please select an option from menu to get started.
+				</p>
+			</div>
+		</Slider>
+
+	)
+}
 
 const HomeMainSection = () => {
 	return (
 		<section className="app-home__content">
-			<h2 className="app-home__title">Welcome to Drinkello</h2>
-			<p className="app-home__description">
-				Drinkello is a recipe finder for classic cocktails. Please select an option from menu to get started.
-			</p>
+			<HomeSlider />
 		</section>
 	);
 }
